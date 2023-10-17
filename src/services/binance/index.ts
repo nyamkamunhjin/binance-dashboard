e52c1008efeb0ce510cb6559eababdf31c90e054
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2023-10-17 15:28:20 +0800

    Change to real account

--- src/services/binance/index.ts
+++ src/services/binance/index.ts
@@ -6,19 +6,19 @@ const apiSecret = process.env.BINANCE_SECRET_KEY;
 
 console.log({ apiKey, apiSecret });
 
-// const binanceClient = Binance({
-//   apiKey,
-//   apiSecret,
-//   getTime: () => moment().unix() * 1000,
-// });
-
 const binanceClient = Binance({
-    apiKey: '8de8820c56ecffb45f724310bbcd521ea17371cd37c09baa96ce1005ed90b387',
-    apiSecret:
-        'eda7f99ab59565689a54519b33ffc4e61339ab305014929463929d6dbb185d99',
+    apiKey,
+    apiSecret,
     getTime: () => moment().unix() * 1000,
-    httpFutures: 'https://testnet.binancefuture.com',
-    wsFutures: 'wss://stream.binancefuture.com',
 });
+// test account
+// const binanceClient = Binance({
+//     apiKey: '',
+//     apiSecret:
+//         '',
+//     getTime: () => moment().unix() * 1000,
+//     httpFutures: 'https://testnet.binancefuture.com',
+//     wsFutures: 'wss://stream.binancefuture.com',
+// });
 
 export { binanceClient };

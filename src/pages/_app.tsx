01f4d26a9db962afb7fbaa020c2a9612703d0723
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2023-10-30 12:30:59 +0800

    Add next auth, mantine ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MantineProvider, createTheme } from '@mantine/core';
 
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
+
+// core styles are required for all packages
+import '@mantine/core/styles.css';
+
+// other css files are required only if
+// you are using components from the corresponding package
+// import '@mantine/dates/styles.css';
+// import '@mantine/dropzone/styles.css';
+// import '@mantine/code-highlight/styles.css';
+// ...
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <MantineProvider>
+            <Component {...pageProps} />;
+        </MantineProvider>
+    );
 }

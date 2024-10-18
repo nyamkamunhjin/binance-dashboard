640d79d7ff437ea8bc5a8c2ce35828b90b250271
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2024-10-18 11:23:50 +0800

    update domain

--- config.ts
+++ config.ts
@@ -3,17 +3,17 @@ import { Connection } from './src/types';
 const configs: Connection[] = [
     {
         name: 'Main',
-        url: 'https://main.munkhjin.online',
+        url: 'https://main.munkhjin.fun',
         symbol: 'SOLUSDT',
     },
     {
         name: 'Trade',
-        url: 'https://trade.munkhjin.online',
+        url: 'https://trade.munkhjin.fun',
         symbol: 'SOLUSDT',
     },
     {
         name: 'Bot',
-        url: 'https://bot.munkhjin.online',
+        url: 'https://bot.munkhjin.fun',
         symbol: 'RUNEUSDT',
     },
 ];
